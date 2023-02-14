54bc0a2478fbe4fff9f6155a3b4b875a339328e8
aplatov76 <aplatov76@gmail.com>
2023-02-14 21:54:50 +0300

    [DP-2] fix router decorator

--- src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
+++ src/shared/config/storybook/RouterDecorator/RouterDecorator.tsx
@@ -1,5 +1,4 @@
 import { Story } from '@storybook/react'
-import { Theme } from 'app/providers/ThemeProvider/lib/ThemeContext'
 import 'app/styles/index.sass'
 import { BrowserRouter } from 'react-router-dom'
 
